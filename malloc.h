042c00504f7048d03607e1fe9c01998a5692bb4d
HINKOKO <piotr62@live.fr>
2023-09-13 14:20:53 +0200

    naive malloc is born from concept page, let's test it!

---
+++ malloc.h
@@ -0,0 +1,22 @@
+#ifndef __MALLOC__
+#define __MALLOC__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* print color for debugging and */
+/* protect eyes from iiiiiiiiiiiniyaaaa */
+#define RED "\033[31m"
+#define COLOR "\033[35m"
+#define COLORDIFF "\033[32m"
+#define COLORALIGN "\033[36m"
+#define RESET "\033[0m"
+
+#define HEADER 0x10 /* 16 bytes of header for each chunk */
+#define ALIGN(size) ((size + (HEADER - 1)) & ~(HEADER - 1))
+
+void *naive_malloc(size_t size);
+
+#endif /* __MALLOC__ */
---
+++ naive_malloc.c
@@ -0,0 +1,85 @@
+#include "malloc.h"
+#define RED "\033[31m"
+#define COLOR "\033[35m"
+#define COLORDIFF "\033[32m"
+#define COLORALIGN "\033[36m"
+#define RESET "\033[0m"
+
+/**
+ * first_call - function to mimic malloc first allocation
+ * of a page-sized block when called.
+ * => Just a piece of code for 'init' the first memory allocation
+ * of size 'page' according to system
+ * @page: size of the page
+ * Return: pointer to the first chunk of allocated page
+ */
+
+void *first_call(ssize_t *page)
+{
+	void *first_chunk;
+
+	(*page) = sysconf(_SC_PAGESIZE);
+	if ((*page) == -1)
+	{
+		perror("sysconf");
+		return (NULL);
+	}
+	first_chunk = sbrk((*page));
+	if (first_chunk == (void *)-1)
+	{
+		perror("sbrk error");
+		return (NULL);
+	}
+	return (first_chunk);
+}
+
+/**
+ * naive_malloc - naive & simple implementation of malloc library call
+ * @size: size needed to be allocated by user
+ *
+ * Return: a pointer to the allocted memory, suitably aligned
+ * FOR ANY KIND OF VARIABLE
+ */
+
+void *naive_malloc(size_t size)
+{
+	static void *block_zero;
+	static size_t chunks;
+	void *block_ptr, *next_block, *payload_addr = NULL;
+	size_t i, std_block_size, block_size, unused_block_size = 0;
+	long pagesize = 0;
+
+	std_block_size = ALIGN(size) + sizeof(size_t);
+
+	if (!chunks)
+		block_zero = first_call(&pagesize);
+
+	if (!block_zero)
+		return (fprintf(stderr, "first_call: failed to allocate page size"), NULL);
+
+	for (i = 0, block_ptr = block_zero; i < chunks; i++)
+	{
+		block_size = *(size_t *)block_ptr;
+		block_ptr = (char *)block_ptr + block_size;
+	}
+
+	unused_block_size = chunks ? *(size_t *)block_ptr : (size_t)pagesize;
+	next_block = (char *)block_ptr + std_block_size;
+
+	while (unused_block_size < (sizeof(size_t) * 2) + ALIGN(size))
+	{
+		pagesize = sysconf(_SC_PAGESIZE);
+		if (pagesize == -1)
+			return (NULL);
+		if (sbrk(pagesize) == (void *)-1)
+			return (NULL);
+		unused_block_size += pagesize;
+	}
+
+	*(size_t *)next_block = unused_block_size - std_block_size;
+	*(size_t *)block_ptr = std_block_size;
+	chunks++;
+
+	payload_addr = (char *)block_ptr + sizeof(size_t);
+	return (payload_addr);
+}
