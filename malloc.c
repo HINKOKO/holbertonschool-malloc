60f3d9b4ca4bd4ee88038940425d7e738d88619e
HINKOKO <piotr62@live.fr>
2023-09-20 15:51:17 +0200

    better vars

--- malloc.c
+++ malloc.c
@@ -1,5 +1,8 @@
 #include "malloc.h"
 
+static void *first_block;
+static size_t chunks;
+
 /**
  * _malloc - reimplementation of malloc
  * @size: bytes requested
@@ -7,44 +10,49 @@
  */
 void *_malloc(size_t size)
 {
-	static void *heap_start;
-	static size_t chunks;
-	void *valid_unused_memory_ptr = NULL, *previous_break = NULL, *ptr = NULL;
-	size_t unused_memory = 0, i = 0, mem_asked = 0, padding = 0;
+	void *payload_addr = NULL, *prev_break = NULL, *ptr = NULL;
+	size_t unused_mem = 0, i = 0, requested = 0, padding = 0;
 
 	if (size == 0)
 		return (NULL);
 
-	if (!heap_start)
+	if (!first_block)
 	{
-		previous_break = sbrk(PAGE);
-		if (previous_break == ((void *)-1))
+		prev_break = sbrk(PAGE);
+		if (prev_break == ((void *)-1))
 			return (NULL);
-		heap_start = previous_break;
-		memset(previous_break, 0, PAGE);
-		*((size_t *)((char *)heap_start)) = PAGE - sizeof(size_t);
+		first_block = prev_break;
+		memset(prev_break, 0, PAGE);
+		*((size_t *)((char *)first_block)) = PAGE - sizeof(size_t);
 	}
 
-	for (i = 0, ptr = heap_start; i < chunks; i++)
+	for (i = 0, ptr = first_block; i < chunks; i++)
+	{
+		/* iterate over the block to find a place to fit */
 		ptr = ((char *)ptr) + *((size_t *)((char *)ptr));
-
-	unused_memory = *((size_t *)((char *)ptr));
-	mem_asked = sizeof(size_t) + size;
-	padding = (sizeof(size_t) - (mem_asked % sizeof(size_t))) % sizeof(size_t);
-	mem_asked += padding;
-
-	while (((int)unused_memory) - ((int)mem_asked) < 0)
-	{
-		previous_break = sbrk(PAGE);
-		if (previous_break == ((void *)-1))
-			return (NULL);
-		memset(previous_break, 0, PAGE);
-		unused_memory += PAGE;
 	}
 
-	*((size_t *)((char *)ptr)) = mem_asked;
-	valid_unused_memory_ptr = (void *)(((char *)ptr) + sizeof(size_t));
-	*((size_t *)(((char *)ptr) + mem_asked)) = unused_memory - mem_asked;
+	unused_mem = *((size_t *)((char *)ptr));
+	requested = sizeof(size_t) + size;
+	padding = (sizeof(size_t) - (requested % sizeof(size_t))) % sizeof(size_t);
+	requested += padding;
+
+	while (((int)unused_mem) - ((int)requested) < 0)
+	{
+		/* when request exceed available unused memory */
+		/* from the previous call to sbrk(PAGE) */
+		prev_break = sbrk(PAGE);
+		/* this check because at some point memory might be exhausted*/
+		if (prev_break == (void *)-1)
+			return (NULL);
+		memset(prev_break, 0, PAGE);
+		/* update your new memory credit accordingly */
+		unused_mem += PAGE;
+	}
+
+	*((size_t *)((char *)ptr)) = requested;
+	payload_addr = (void *)(((char *)ptr) + sizeof(size_t));
+	*((size_t *)(((char *)ptr) + requested)) = unused_mem - requested;
 	chunks++;
-	return (valid_unused_memory_ptr);
+	return (payload_addr);
 }
